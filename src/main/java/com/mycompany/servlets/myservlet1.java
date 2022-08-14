c5f54f0634cb4fdaaa9f40963022852132296c83
Prasanth Kumar K <starworse@yahoo.com>
2022-08-14 13:29:59 +0530

    Struts1 project with Maven(only for build)

---
+++ src/main/java/com/mycompany/servlets/myservlet1.java
@@ -0,0 +1,15 @@
+package com.mycompany.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class myservlet1 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Servlet Loaded...");
+    }
+}
